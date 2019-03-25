eac7d67150eda725002a15723abfa564b54c557a
Dariell Vasquez <dariellv@yahoo.com>
2019-03-25 15:07:23 -0400

    min max normalization

--- score.js
+++ score.js
@@ -71,3 +71,17 @@ function splitDataSet (data, testCount) {
 
   return [testSet, trainingSet];
 }
+
+function minMax(data, featureCount) {
+  const clonedData = _.cloneDeep(data);
+  for(let i = 0; i < featureCount; i++){
+    const column = clonedData.map(row => row[i]);
+    const min = _.min(column);
+    const max = _.max(column);
+
+    for(let j = 0; j < column.length; j++){
+      clonedData[j][i] = (clonedData[j][i]- min) / (max-min);
+    }
+  }
+  return clonedData;
+}
